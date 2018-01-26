3e4e94eb803f0073a708e9f7fbe9218c8fb4dc9d
brainspork <silv.amee@gmail.com>
2018-01-26 15:52:59 -0800

    Initial build

--- server.js
+++ server.js
@@ -19,5 +19,13 @@ const forceSSL = function() {
   // to use the forceSSL
   // middleware
   app.use(forceSSL());
+
+  const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
   
 app.listen(process.env.PORT || 8080);
